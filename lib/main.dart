3d37835c95d6070fed6303ae6ada985847b2ef30
bluedaemondev <blackmoodgames@gmail.com>
2025-03-06 17:17:38 -0300

    (ADD) BigCard Widget

--- lib/main.dart
+++ lib/main.dart
@@ -27,21 +27,56 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    print("You are requesting a new WordPair");
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var currentPair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(onPressed: () {print('button was pressed');}, child: Text('Next')),
-        ],
+        body: SizedBox.expand(
+            child: Column(
+      children: [
+        Text('My Awesome Random Incredible idea:'),
+        BigCard(currentPair: currentPair),
+        ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next')),
+      ],
+    )));
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.currentPair,
+  });
+
+  final WordPair currentPair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Card(
+      color: theme.colorScheme.primary,    // ← And also this.
+      child: Padding(
+        padding: const EdgeInsets.all(200),
+        child: Text(currentPair.asLowerCase),
       ),
     );
+
+    // return Text(currentPair.asLowerCase);
   }
 }
